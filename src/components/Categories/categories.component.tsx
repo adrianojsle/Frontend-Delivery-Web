c2604d788a9c8690821834acb8e00558379131e6
adrianojsle <oadrianoleite@gmail.com>
2023-05-09 12:05:32 -0300

    Icones na sessão de categorias

--- src/components/Categories/categories.component.tsx
+++ src/components/Categories/categories.component.tsx
@@ -1,18 +1,27 @@
 import TitleSection from "../TitleSection/title-section.component";
 import ItemCategory from "./parts/item-category.part";
+import Novidades from "../../assets/icons/novidades.png";
+import Pizzas from "../../assets/icons/pizzas.png";
+import Salgados from "../../assets/icons/salgados.png";
+import Doces from "../../assets/icons/doces.png";
+import Bebidas from "../../assets/icons/bebidas.png";
+import Panquecas from "../../assets/icons/panquecas.png";
+import Sorvetes from "../../assets/icons/sorvetes.png";
+import Saladas from "../../assets/icons/saladas.png";
+
 
 export default function Categories() {
     return <div>
         <TitleSection firstWord="Menu de " secondWord="Categorias" />
         <div className="mt-6 flex flex-row">
-            <ItemCategory title="Novidades" active={true} />
-            <ItemCategory title="Pizzas" active={false} />
-            <ItemCategory title="Salgados" active={false} />
-            <ItemCategory title="Doces" active={false} />
-            <ItemCategory title="Bebidas" active={false} />
-            <ItemCategory title="Cafés" active={false} />
-            <ItemCategory title="Sorvetes" active={false} />
-            <ItemCategory title="Saladas" active={false} />
+            <ItemCategory title="Novidades" active={true} icon={Novidades} />
+            <ItemCategory title="Pizzas" active={false} icon={Pizzas} />
+            <ItemCategory title="Salgados" active={false} icon={Salgados} />
+            <ItemCategory title="Doces" active={false} icon={Doces} />
+            <ItemCategory title="Bebidas" active={false} icon={Bebidas} />
+            <ItemCategory title="Panquecas" active={false} icon={Panquecas} />
+            <ItemCategory title="Sorvetes" active={false} icon={Sorvetes} />
+            <ItemCategory title="Saladas" active={false} icon={Saladas} />
         </div>
     </div>
 }
--- src/components/Categories/parts/item-category.part.tsx
+++ src/components/Categories/parts/item-category.part.tsx
@@ -1,24 +1,25 @@
 type Props = {
     title: string;
     active: boolean;
+    icon: string;
 }
 
-export default function ItemCategory({title, active}: Props) {
+export default function ItemCategory({title, active, icon}: Props) {
     return active ? <div className="flex flex-col justify-center items-center w-24 py-3 bg-secondary rounded-lg mr-5">
-        <div className="w-12 h-12 bg-white rounded-lg">
-
+        <div className="w-12 h-12 bg-white rounded-lg flex justify-center items-center">
+            <img src={icon} className="w-6 h-6" />
         </div>
         <div className="mt-1">
-            <span className="text-black text-xs">
+            <span className="text-black text-xs font-semibold">
                 {title}
             </span>
         </div>
     </div> : <div className="flex flex-col justify-center bg-white border border-gray-100 items-center w-24 py-3 rounded-lg mr-5">
-        <div className="w-12 h-12 border border-gray-200 rounded-lg">
-
+        <div className="w-12 h-12 border border-gray-200 rounded-lg flex justify-center items-center">
+        <img src={icon} className="w-6 h-6" />
         </div>
         <div className="mt-1">
-            <span className="text-gray-400 text-xs">
+            <span className="text-gray-400 text-xs font-semibold">
                 {title}
             </span>
         </div>
