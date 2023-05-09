ae905d8fe0eed276a81f64658ddf4cb29381f9b6
adrianojsle <oadrianoleite@gmail.com>
2023-05-09 11:16:53 -0300

    Correção de nomeclatura

--- src/components/Sidebar/parts/item-sidebar.part.tsx
+++ src/components/Sidebar/parts/item-sidebar.part.tsx
@@ -4,7 +4,7 @@ type Props = {
     active: boolean;
 }
 
-export default function Menu({ icon, label, active }: Props) {
+export default function ItemSidebar({ icon, label, active }: Props) {
     return active ? <div className="w-20 py-3 bg-primary flex flex-col justify-center items-center rounded-xl shadow-md mb-5">
         <img src={icon} className="w-6 h-6" />
         <span className="text-xs text-white mt-1">{label}</span>
--- src/components/Sidebar/sidebar.component.tsx
+++ src/components/Sidebar/sidebar.component.tsx
@@ -1,21 +1,21 @@
 import Logo from "../Logo/logo.component";
-import Menu from "./parts/menu.part";
 import Home from "../../assets/icons/home.png";
 import Vendas from "../../assets/icons/vendas.png";
 import Pedidos from "../../assets/icons/pedidos.png";
 import Mensagens from "../../assets/icons/mensagens.png";
 import Configuracoes from "../../assets/icons/configuracoes.png";
 import Sair from "../../assets/icons/sair.png";
+import ItemSidebar from "./parts/item-sidebar.part";
 
 export default function Sidebar() {
     return <div className="w-1/12 bg-white p-6">
         <Logo />
         <div className="flex flex-col justify-center items-center pt-6">
-            <Menu icon={Home} label="Home" active={true} />
-            <Menu icon={Vendas} label="Vendas" active={false} />
-            <Menu icon={Mensagens} label="Mensagens" active={false} />
-            <Menu icon={Pedidos} label="Pedidos" active={false} />
-            <Menu icon={Configuracoes} label="Configurações" active={false} />
+            <ItemSidebar icon={Home} label="Home" active={true} />
+            <ItemSidebar icon={Vendas} label="Vendas" active={false} />
+            <ItemSidebar icon={Mensagens} label="Mensagens" active={false} />
+            <ItemSidebar icon={Pedidos} label="Pedidos" active={false} />
+            <ItemSidebar icon={Configuracoes} label="Configurações" active={false} />
         </div>
         <div className="flex grow mt-16">
             <div className="w-20 py-2 flex flex-col justify-center items-center mb-4">
